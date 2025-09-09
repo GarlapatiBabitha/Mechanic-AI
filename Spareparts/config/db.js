1e5de5f3a0c5abf247b89b38047f5d383bc5c589
babitha <babithareddygarlapati@gmail.com>
2025-09-09 22:28:30 +0530

    Database correction

--- Spareparts/config/db.js
+++ Spareparts/config/db.js
@@ -1,8 +1,7 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
 
-// mongoose.connect("mongodb+srv://srihari:srihari123@cluster0.lswdt.mongodb.net/SparePartsDB?retryWrites=true&w=majority&appName=Cluster0")
-mongoose.connect("mongodb+srv://srihari:srihari123@cluster0.lswdt.mongodb.net/")
+mongoose.connect("mongodb+srv://srihari:srihari123@cluster0.lswdt.mongodb.net/SparePartsDB?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>{
     console.log("Connected to mongo DB");
 }).catch((err)=>{
